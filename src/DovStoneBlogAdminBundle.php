92063bea5d536d1f18e5c6839cbfe4b5c891d194
stOne <silveredovoui@gmail.com>
2021-03-03 15:33:12 +0000

    Add files via upload

---
+++ src/DovStoneBlogAdminBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stOne
+ * Date: 24/02/2018
+ * Time: 14:33
+ */
+
+namespace DovStone\Bundle\BlogAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DovStoneBlogAdminBundle extends Bundle
+{
+}
